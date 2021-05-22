1f8bfe408cc06c46064e2e1d2c51f6bab495caf8
Дудко Михаил <mikel2000@bk.ru>
2021-05-22 19:44:20 +0500

    Добавлен клиентский проект, и проект для TCP сервера

--- TCPServer/Program.cs
+++ TCPServer/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace MegaLab
+namespace TCPServer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hello from server!");
         }
     }
 }
