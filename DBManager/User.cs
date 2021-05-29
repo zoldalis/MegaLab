511faf753aafd2d1e9ac9510479e8eb74491e2c1
mikel <dudkomikhail00@gmail.com>
2021-05-29 20:24:12 +0500

    для базы

---
+++ DBManager/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBManager
+{
+
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=4444;Database=usersdb;Username=postgres;Password=12345");
+        }
+    }
+}
