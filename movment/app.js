40b890403f2f6e01c431e95e25deb327e5e7bb73
litresov <44399362+litresov@users.noreply.github.com>
2021-06-25 17:38:07 +0500

    1

--- movment/app.js
+++ movment/app.js
@@ -1,17 +1,37 @@
-var net = require('net');
+var net = require('net'); // сетевая библиотека
+const fs = require("fs"); // библиотека filestream для работы с файлами
+var client = new net.Socket(); // создаем соект для клиента
+var time_delay = 1000; // задержка между сообщениями
+var sensitivity = 1000; // fake настройка для датчика
+var move_Flag = false; // флаг движения (false - движения нет, true - движение есть)
+var fileContent = fs.readFileSync("settings.txt", "utf8").replace('\n', ''); // при запуске контроллера устанавливаем значение гуида из файла
+var guid = fileContent;
+var message = guid + '|send_data|'; // заготовка сообщения
+setInterval(myFunc, time_delay); // задаем интервал отправки сообщения и функию которая будет постоянно выполняться
 
-var client = new net.Socket();
-
-client.connect(4040, '127.0.0.1', function () {
-	console.log('Connected');
-	client.write('Hello, server! Love, Client.');
+client.connect(1337, '127.0.0.1', function () {
+	console.log('Connected');                        // событие подключения в серверу
+	client.write(guid + '|' + 'get_settings' + '|'); // при подключении отправляем запрос на настройки
 });
-
+function myFunc() {
+	FakeSens();
+	console.log('Client message: ' + message + move_Flag); // основная функция отправки сообщения
+	client.write(guid + "|send_data|" + move_Flag);
+}
+function randomIntFromInterval(min, max) { // функция рандома в промежутке от мин до макс включительно
+	return Math.floor(Math.random() * (max - min + 1) + min)
+}
+function FakeSens() { // функция которая при вызове сверяет *sensitivity* с рандомным числом от 100 до 2000. и в зависимости от результата меняет флаг движения
+	if (sensitivity > randomIntFromInterval(100, 2000)) {
+		move_Flag = true;
+	}
+	else move_Flag = false;
+}
 client.on('data', function (data) {
-	console.log('Received: ' + data);
-	client.destroy(); // kill client after server's response
+	console.log('Received: ' + data + '\n');											// событие принятия сообщения от сервера
+	sensitivity = data;
 });
-
 client.on('close', function () {
-	console.log('Connection closed');
+	console.log('Connection closed');													// событие закрытия подключения
+	fs.writeFileSync("settings.txt", guid + "\n" + time_delay + "\n" + sensitivity)     // при закрытии подключения записываем переменные в файл
 });
